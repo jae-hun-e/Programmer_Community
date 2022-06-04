d26ffa4a92b769450e144703b92dc4c6207ff1cd
jae-hun-e <wognskec@hanyang.ac.kr>
2022-06-04 15:23:03 +0900

    admin 푸가

--- snsapp/admin.py
+++ snsapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, FreePost, FreeComment
 
 
 admin.site.register(Post)
 admin.site.register(Comment)
-# Register your models here.
+admin.site.register(FreePost)
+admin.site.register(FreeComment)
